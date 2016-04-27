560a8ec25470a544c2920f230cbdc1cf5e2408f9
Alex Korsakov <aleksey.korsakov@outlook.com>
2016-04-27 11:00:28 +0300

    Plants vs. Zombies clone
    v. 1.00

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level auto load disabled, use a postive number is seconds");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+    void Update()
+    {
+        ExitWithKey();
+    }
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+    public void ExitWithKey() //temporary for demonstration
+    {
+        if (Input.GetKey(KeyCode.Escape) && (Application.loadedLevelName != "01a Start"))
+        {
+            LoadLevel("01a Start");
+        }
+    }
+}
